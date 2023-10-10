201b21eefe43545149ec5a3fc81e3195d12cdd09
fishlands <fishninjas3@gmail.com>
2023-10-10 23:41:52 +0300

    Initial Commit

---
+++ hellostuff.py
@@ -0,0 +1,26 @@
+
+import os
+os.system('clear')
+
+##########################
+# Data Types
+# Strings
+# Numbers
+# Lists
+# Tuplets
+# Dictionaries
+# Boolean
+##########################
+
+
+
+first_name = 'Mathew'
+age = 44
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"
+}
+name = True
+
+print(name)
